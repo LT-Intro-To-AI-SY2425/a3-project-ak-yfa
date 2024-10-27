9b57ade766bb338128d5bd5d57b52408c5420d61
yyfigueroa <yyfigueroa@cps.edu>
2024-10-27 16:53:06 -0500

    Update songs.py

--- songs.py
+++ songs.py
@@ -1,1 +1,72 @@
+from typing import List, Tuple
 
+song_db: List[Tuple[str, str, int, List[str]]] = [
+  (
+    "bad romance",
+    "Lady Gaga",
+    2010,
+  ),
+  (
+    "just the way you are",
+    "bruno mars",
+    2010,
+  ), 
+  (
+    "rolling in the deep",
+    "adele", 
+    2011,
+  ), 
+  (
+    "pumped up kicks",
+    "foster the people", 
+    2011,
+  ), 
+  (
+    "everybody talks", 
+    "neon trees", 
+    2012,
+  ),
+  ( 
+    "video games", 
+    "lana del rey", 
+    2012, 
+  ), 
+  ( 
+    "mirrors", 
+    "justin timberlake", 
+    2013, 
+  ), 
+  ( 
+    "hold on, we're going home", 
+    "drake", 
+    2013, 
+  ), 
+  (
+    "get lucky", 
+    "daft punk", 
+    2013, 
+  ), 
+  (
+    "loyal", 
+    "chris brown", 
+    2014, 
+  ), 
+  (
+    "don't", 
+    "bryson tiller", 
+    2015, 
+  ),
+    "in the night", 
+    "the weeknd",
+    2015, 
+  ), 
+  (
+    "the less i know better", 
+    "tame impala", 
+    2015,
+  ), 
+]
+
+
+  
+    
